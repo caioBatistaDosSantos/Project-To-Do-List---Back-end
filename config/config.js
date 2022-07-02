01b785e45a3f7c7dd8e1d5afdcddcab927b57040
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-02 20:13:25 -0300

    fix: correcting bug in variavel HOTSNAME

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
 
 const options = {
-  host: process.env.MYSQL_HOST || 'localhost',
+  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
-  database: process.env.MYSQL_DB_NAME || 'TO_DO_DB',
+  database: `${process.env.MYSQL_DB_NAME || 'TO_DO_DB'}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
