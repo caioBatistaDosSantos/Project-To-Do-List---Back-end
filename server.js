e3bfb4746f4102e756d24259b66cb12f66b87305
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-02 20:56:51 -0300

    feat: added var PORT in server.js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+require('dotenv').config();
 const routes = require('./routes/index');
 
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
