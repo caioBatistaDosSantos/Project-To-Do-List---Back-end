d745267bf8812e222560ab55f1d39ac84a434dd1
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-07-02 23:46:39 -0300

    test: add success case to validateTask.test.js

--- test/unit/validateTask.test.js
+++ test/unit/validateTask.test.js
@@ -3,7 +3,7 @@ const { expect } = require('chai');
 const sinon = require('sinon');
 const validateTask = require('../../middlewares/validateTask');
 
-describe('Testando casos de erro no middleware validateTask:', () => {
+describe('Testando o middleware validateTask:', () => {
   describe('Quando "task" não é encontrado:', () => {
     const req = { body: { status: 'pendente' } };
     const res = {};
@@ -93,6 +93,24 @@ describe('Testando casos de erro no middleware validateTask:', () => {
       await validateTask(req, res, next);
 
       expect(res.json.calledWith({ message: '"status" length must be at least 5 characters long' })).to.be.equal(true);
+    });
+  });
+
+  describe('Testando caso de sucesso:', () => {
+    const req = { body: { task: 'Task example 1', status: 'pendente' } };
+    const res = {};
+    const next = () => {};
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns(res);
+    });
+
+    it('não retorna status ou mensagens de erro', async () => {
+      await validateTask(req, res, next);
+
+      expect(res.status.calledWith(400)).to.be.equal(false);
+      expect(res.json.calledWith({ message: '*' })).to.be.equal(false);
     });
   });
 });
